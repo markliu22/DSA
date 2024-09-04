42dfe3891e53444d97d6f3fa50dc5e15ed78d907
Mark Liu <markliu202@gmail.com>
2024-09-04 23:26:02 +0800

    a

---
+++ DataStructures/MonotonicQueue.cc
@@ -0,0 +1,10 @@
+class MonotonicQueue {
+private:
+    deque<int> data;
+public:
+    void push(int n) {
+        while (!data.empty() && data.back() < n) 
+            data.pop_back();
+        data.push_back(n);
+    }
+};

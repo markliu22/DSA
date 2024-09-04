5a29eebcc12f6a3830a8958afa5d3d55fdcd3414
Mark Liu <markliu202@gmail.com>
2024-09-04 23:28:05 +0800

    a

--- DataStructures/Stack.java
+++ DataStructures/Stack.java
@@ -1,19 +1,35 @@
-// ref: https://courses.cs.washington.edu/courses/cse373/13wi/lectures/01-23/
-// Note: Generics are the closest thing Java has to C++ templates
-// ref: https://stackoverflow.com/questions/2159338/what-is-the-java-equivalent-of-cs-templates
-/*
-Difference: 
-In C++, what happens is that the compiler creates a compiled version of the template for all instances of the template used in code. 
-In Java, the compiler creates only one compiled version of the code regardless of how many times it is used with different type parameters.
-*/
+// Stack is usually implemented using array or linked list. 
+// This is the linked list implementation from Cracking the Coding Interview
 
-// This interface defines a Stack abstract data type (ADT) allowing adding/removing items in FIFO from the "top" of the stack
-// Recall abstract classes cannot be instantiated. We initialize an ArrayStack instead of a Stack
+public class Stack {
 
-public interface Stack<E> {
-	public void push(E value);
-	public E pop();
-	public E peek();
-	public boolean isEmpty();
-	public int size();
+  private static class Node {
+    private int data;
+    private Node next;
+    private Node(int data) {
+      this.data = data;
+    }
+  }
+
+  private Node top;
+
+  public boolean isEmpty() {
+    return top == null;
+  }
+
+  public int peek() {
+    return top.data;
+  }
+
+  public void push(int data) {
+    Node n = new Node(data);
+    n.next = top;
+    top = n;
+  }
+
+  public int pop() {
+    int data = top.data;
+    top = top.next;
+    return data;
+  }
 }
